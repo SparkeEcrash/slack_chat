6cc46fe01b278ea164f169f168e853a31ae7f5db
SparkeEcrash <jpark121989@gmail.com>
2019-05-05 00:22:30 -0700

    add messaging capability

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -13,13 +13,41 @@ const user_reducer = (state = initialUserState, action) => {
 				currentUser: action.payload.currentUser,
 				isLoading: false
 			}
+		case actionTypes.CLEAR_USER: 
+			return {
+				...state,
+				isLoading: false
+			}
 		default:
+			return state;
+	}
+};
+
+const initialChannelState = {
+	currentChannel: null,
+	isPrivateChannel: false
+}
+
+const channel_reducer = (state = initialChannelState, action) => {
+	switch(action.type) {
+		case actionTypes.SET_CURRENT_CHANNEL:
+			return {
+				...state,
+				currentChannel: action.payload.currentChannel
+			}
+		case actionTypes.SET_PRIVATE_CHANNEL:
+			return {
+				...state,
+				isPrivateChannel: action.payload.isPrivateChannel
+			}
+		default: 
 			return state;
 	}
 }
 
 const rootReducer = combineReducers({
-	user: user_reducer
+	user: user_reducer,
+	channel: channel_reducer
 });
 
 export default rootReducer;
